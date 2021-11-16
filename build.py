54febd0ae13c03c3157a4cbc64390d59dcc1c6bc
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2021-11-16 20:02:40 +0000

    Moved exe building onto `tools/`. Added `util` module and improved logging.

--- build.py
+++ build.py
@@ -1,161 +1,25 @@
 #!/bin/env python3
 
 # Imports
-import os
 import io
+import os
 import subprocess
 from scoping import scoping
 import shutil
-import glob
 import pathlib
-from tools import generate_linker_fns
-from tools import preprocess_asm_file
+from tools import build_exe, util
 
 # Create the build folders if they don't exist
 for path in ["build", "build/llvm", "build/asm", "build/rust", "build/iso", "build/bin"]:
 	os.makedirs(path, exist_ok=True)
 
+# Build the executable
+build_exe.build()
+
 # Compile all binaries for the rest of the build process
 for bin_name in ["dcb-mkdrv"]:
 	args = ["cargo", "build", "--release", "-p", f"{bin_name}", "-Z", "unstable-options", "--out-dir", "build/bin"]
-	print(f" !Compiling tool `{bin_name}`")
-	subprocess.run(args, check=True)
-
-# Compile `dcb-macros` onto a `so`
-# Note: Not `--release` because it takes a bit
-with scoping():
-	args = ["cargo", "build", "-p", "dcb-macros", "-Z", "unstable-options", "--out-dir", "build/rust"]
-	print(" !Compiling `dcb-macros`")
-	subprocess.run(args, check=True)
-
-# Compile `core-impl` onto a `rlib`
-with scoping():
-	args = [
-	    "cargo", "build", "--release", "-p", "core-impl", "-Z", "unstable-options", "--out-dir", "build/rust",
-	    "--target=mipsel-sony-psx.json"
-	]
-	print(" !Compiling `core-impl`")
-	subprocess.run(args, check=True)
-
-# Compile `dcb-rs` onto `llvm-ir`
-with scoping():
-	args = [
-	    "rustc", "--crate-name", "dcb", "--edition=2021", "dcb-rs/src/lib.rs", "--emit=llvm-ir", "-C", "opt-level=3",
-	    "-C", "embed-bitcode=no", "-C", "extra-filename=-rs", "--target=mipsel-sony-psx.json", "--out-dir", "build/llvm/",
-	    "--extern", "core_impl=build/rust/libcore_impl.rlib", "--extern", "dcb_macros=build/rust/libdcb_macros.so"
-	]
-	print(" !Compiling `dcb-rs`")
-	subprocess.run(args, check=True)
-
-# Compile all `llvm-ir` onto `asm`
-with scoping():
-	# Run the C preprocessor on the llvm input
-	# TODO: Check if this is actually a good idea, doesn't feel like it
-	# TODO: Maybe don't ignore ALL warnings
-	print(" !Preprocessing llvm")
-	with scoping():
-		args = ["cpp", "-E", "-P", "-w", "dcb-llvm/dcb.ll", "-o", "build/llvm/dcb-llvm.ll"]
-		subprocess.run(args, check=True)
-
-	# Replace any `mips2` with `mips1` in the rust output
-	# TODO: Do this properly somehow
-	print(" !Replacing `mips2` with `mips1` in dcb-rs")
-	with scoping():
-		args = ["sed", "-i", "-e", "s/mips2/mips1/g", "build/llvm/dcb-rs.ll"]
-		subprocess.run(args, check=True)
-
-	# Then link both together
-	print(" !Linking llvm")
-	with scoping():
-		args = [
-		    "/opt/llvm-mips1/bin/llvm-link",
-		    "-S",
-		    "-o=build/llvm/dcb.ll",
-		    "build/llvm/dcb-rs.ll",
-		    "build/llvm/dcb-llvm.ll",
-		]
-		subprocess.run(args, check=True)
-
-	# Then compile them
-	print(" !Compiling llvm")
-	with scoping():
-		args = [
-		    "/opt/llvm-mips1/bin/llc",
-		    "build/llvm/dcb.ll",
-		    "-O0",
-		    "-march=mips",
-		    "-mcpu=mips1",
-		    "-mattr=+mips1,+soft-float",
-		    "-o=build/asm/dcb-llvm.s",
-		]
-		subprocess.run(args, check=True)
-
-# Preprocss all assembly files onto `build/asm/dcb-asm`
-with scoping():
-	print(" !Preprocessing asm")
-	for path, _, files in os.walk("dcb-asm"):
-		for file in files:
-			if not file.endswith(".s"):
-				continue
-
-			file_path = os.path.join(path, file)
-			output_path = str("build/asm" / pathlib.Path(file_path))
-			preprocess_asm_file.main(file_path, output_path)
-
-# Now compile all `asm` into object files
-with scoping():
-	print(" !Compiling asm")
-	args = [
-	    "mips-linux-gnu-as", "-o", "build/dcb.o", "-EL", "-mips1", "-march=r3000", "-O2", "build/asm/dcb-llvm.s",
-	    "build/asm/dcb-asm/dcb.s"
-	]
-
-	process = subprocess.Popen(args, stderr=subprocess.PIPE)
-
-	for line in io.TextIOWrapper(process.stderr, encoding="utf-8"):
-		line = line.replace("build/asm/dcb-asm", "dcb-asm")
-		print(line, end="")
-
-	process.wait()
-	if process.returncode != 0:
-		exit(1)
-
-# Then generate all the linker functions
-print(" !Generating linker functions")
-generate_linker_fns.main()
-
-# Then link the archive
-with scoping():
-	args = [
-	    "mips-linux-gnu-ld",
-	    "--whole-archive",
-	    "build/dcb.o",
-	    "-o",
-	    "build/dcb.bin",
-	    "-EL",
-	    "--nmagic",
-	    "--script",
-	    "psx.ld",
-	    "--warn-section-align",
-	]
-	print(" !Linking object files")
-	subprocess.run(args, check=True)
-
-# Convert it into a psexe
-with scoping():
-	args = [
-	    "elf2psexe",
-	    "NA",
-	    "build/dcb.bin",
-	    "build/SLUS_013.28",
-	]
-	print(" !Converting elf to psexe")
-	subprocess.run(args, check=True)
-
-# Then make sure they're equal
-with scoping():
-	args = ["diff", "build/SLUS_013.28", "build/original/SLUS_013.28"]
-	print(" !Comparing psexe")
+	util.log(f"Compiling tool `{bin_name}`")
 	subprocess.run(args, check=True)
 
 # Copy all files in `dcb` to make the ISO, packing any directories into `DRV`s
@@ -165,7 +29,7 @@ for entry in os.scandir("dcb/"):
 		continue
 
 	args = ["build/bin/dcb-mkdrv", "--quiet", f"{entry.path}", "-o", f"build/iso/{entry.name}.DRV"]
-	print(f" !Packing drv: `{entry.name}`")
+	util.log(f"Packing drv: `{entry.name}`")
 	subprocess.run(args, check=True)
 
 # Make sure they're equal
@@ -173,6 +37,6 @@ for entry in os.scandir("dcb/"):
 """
 for entry in os.scandir("build/iso"):
 	args = ["diff", f"{entry.path}", f"build/original/iso/{entry.name}"]
-	print(f" !Comparing `{entry.name}`")
+	util.log(f"Comparing `{entry.name}`")
 	subprocess.run(args, check=True)
 """
