0d241d4435c690b51414752615e63de707b8d7bf
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2021-11-19 18:23:41 +0000

    Added `drv-undrv` tool. Added drv and iso timestamps for creating the files.

---
+++ tools/dcb-undrv/src/args.rs
@@ -0,0 +1,93 @@
+//! Cli manager
+
+// Imports
+use clap::{App as ClapApp, AppSettings, Arg as ClapArg};
+use std::path::PathBuf;
+
+/// Data from the command line
+#[derive(PartialEq, Clone, Debug)]
+pub struct Args {
+	/// Input files
+	pub input_files: Vec<PathBuf>,
+
+	/// The output directory
+	pub output_dir: Option<PathBuf>,
+
+	pub quiet: bool,
+
+	pub warn_on_override: bool,
+}
+
+/// Returns all arguments
+	pub fn get() -> Args {
+		const INPUT_FILES_STR: &str = "input-files";
+		const OUTPUT_DIR_STR: &str = "output-dir";
+		const QUIET_STR: &str = "quiet";
+		const WARN_ON_OVERRIDE_STR: &str = "warn-on-override";
+
+		// Get all matches from cli
+		let matches = ClapApp::new("Drv Extractor")
+			.version("0.1")
+			.author("Filipe Rodrigues <filipejacintorodrigues1@gmail.com>")
+			.about("Extractor for `drv` files.")
+			.setting(AppSettings::ArgRequiredElseHelp)
+			.arg(
+				ClapArg::with_name(INPUT_FILES_STR)
+					.help("The input files to use")
+					.required(true)
+					.multiple(true),
+			)
+			.arg(
+				ClapArg::with_name(OUTPUT_DIR_STR)
+					.help("The directory to output to")
+					.long_help(
+						"The directory to output to. If not specified, the parent of the input file is used. If it \
+						 doesn't exist, the current directory is used",
+					)
+					.short("o")
+					.long("output-dir")
+					.takes_value(true),
+			)
+			.arg(
+				ClapArg::with_name(QUIET_STR)
+					.help("Suppresses listing of extracted files")
+					.long_help(
+						"Suppresses printing on `stdout` of this program. If any errors or warnings occur, they will \
+						 still be printed to stderr",
+					)
+					.short("q")
+					.long("quiet"),
+			)
+			.arg(
+				ClapArg::with_name(WARN_ON_OVERRIDE_STR)
+					.help("Warns when overriding files that already exist.")
+					.long_help(
+						"Warns if this program would override existing files. By default no warnings are produced.",
+					)
+					.long("warn-on-override"),
+			)
+			.get_matches();
+
+		// Get the input filename
+		// Note: required
+		let input_files: Vec<_> = matches
+			.values_of(INPUT_FILES_STR)
+			.expect("Unable to get required argument `input-file`")
+			.map(PathBuf::from)
+			.collect();
+
+
+		let output_dir = matches.value_of(OUTPUT_DIR_STR).map(PathBuf::from);
+
+		let quiet = matches.is_present(QUIET_STR);
+
+		let warn_on_override = matches.is_present(WARN_ON_OVERRIDE_STR);
+
+		// Return the data
+		Args {
+			input_files,
+			output_dir,
+			quiet,
+			warn_on_override,
+		}
+	}
---
+++ tools/dcb-undrv/src/main.rs
@@ -0,0 +1,157 @@
+//! `.DRV` extractor
+
+// Modules
+mod args;
+
+// Imports
+use self::args::Args;
+use anyhow::Context;
+use dcb_drv::{DirEntryKind, DirPtr};
+use std::{
+	fs, io,
+	path::{Path, PathBuf},
+};
+
+fn main() -> Result<(), anyhow::Error> {
+	// Initialize the logger
+	simplelog::TermLogger::init(
+		log::LevelFilter::Info,
+		simplelog::Config::default(),
+		simplelog::TerminalMode::Stderr,
+	)
+	.expect("Unable to initialize logger");
+
+	// Get all args
+	let args = args::get();
+
+	// For each input file, extract it
+	for input_file_path in &args.input_files {
+		// If we don't have an output, try the input filename without extension if it's `.drv`, else use `.`
+		let output_dir = match &args.output_dir {
+			Some(output) => output.clone(),
+			None => match input_file_path.extension() {
+				Some(extension) if extension.eq_ignore_ascii_case("drv") => input_file_path.with_extension(""),
+				_ => PathBuf::from("."),
+			},
+		};
+
+		// Open the file and parse a `drv` filesystem from it.
+		let input_file = fs::File::open(&input_file_path).context("Unable to open input file")?;
+		let input_file_metadata = input_file.metadata().context("Unable to get input file metadata")?;
+		let mut input_file = io::BufReader::new(input_file);
+
+		// Create output directory if it doesn't exist
+		zutil::try_create_folder(&output_dir)
+			.with_context(|| format!("Unable to create directory {}", output_dir.display()))?;
+
+		// Then extract the tree
+		if let Err(err) = self::extract_tree(&mut input_file, DirPtr::root(), &output_dir, &args) {
+			log::error!("Unable to extract files from {}: {:?}", input_file_path.display(), err);
+		}
+
+		// And set it's time to the input file
+		let time = filetime::FileTime::from_last_modification_time(&input_file_metadata);
+		if let Err(err) = filetime::set_file_mtime(&output_dir, time) {
+			log::warn!(
+				"Unable to write date for output directory {}: {}",
+				output_dir.display(),
+				zutil::fmt_err_wrapper(&err)
+			);
+		}
+	}
+
+	Ok(())
+}
+
+/// Extracts a `.drv` file from a reader and starting directory
+fn extract_tree<R: io::Read + io::Seek>(
+	reader: &mut R, dir_ptr: DirPtr, path: &Path, args: &Args,
+) -> Result<(), anyhow::Error> {
+	// Get all entries
+	// Note: We need to collect to free the reader so it can seek to the next files.
+	let entries: Vec<_> = dir_ptr
+		.read_entries(reader)
+		.with_context(|| format!("Unable to get directory entries of {}", path.display()))?
+		.collect();
+
+	// Then extract each entry
+	for entry in entries {
+		// If we can't read it, return Err
+		let entry = entry.with_context(|| format!("Unable to read directory entry of {}", path.display()))?;
+
+		// Create the date
+		// Note: `.DRV` only supports second precision.
+		let time = filetime::FileTime::from_unix_time(entry.date.timestamp(), 0);
+
+		// Then check it's type
+		match entry.kind {
+			// If it's a file, create the file and write all contents
+			DirEntryKind::File { extension, ptr } => {
+				let path = path.join(format!("{}.{}", entry.name, extension));
+
+				// Log the file and it's size
+				if !args.quiet {
+					println!(
+						"{} ({}B)",
+						path.display(),
+						size_format::SizeFormatterSI::new(u64::from(ptr.size))
+					);
+				}
+
+				// If the output file already exists, log a warning
+				if args.warn_on_override && path.exists() {
+					log::warn!("Overriding file {}", path.display());
+				}
+
+				// Get the file's reader.
+				let mut file_reader = ptr
+					.cursor(&mut *reader)
+					.with_context(|| format!("Unable to read file {}", path.display()))?;
+
+				// Then create the output file and copy.
+				let mut output_file =
+					fs::File::create(&path).with_context(|| format!("Unable to create file {}", path.display()))?;
+				std::io::copy(&mut file_reader, &mut output_file)
+					.with_context(|| format!("Unable to write file {}", path.display()))?;
+
+				// And set the file's modification time
+				if let Err(err) = filetime::set_file_handle_times(&output_file, None, Some(time)) {
+					log::warn!(
+						"Unable to write date for file {}: {}",
+						path.display(),
+						zutil::fmt_err_wrapper(&err)
+					);
+				}
+			},
+
+			// If it's a directory, create it and recurse for all it's entries
+			DirEntryKind::Dir { ptr } => {
+				let path = path.join(entry.name.as_str());
+
+				// Log the directory
+				if !args.quiet {
+					println!("{}/", path.display());
+				}
+
+				// Create the directory and recurse over it
+				zutil::try_create_folder(&path)
+					.with_context(|| format!("Unable to create directory {}", path.display()))?;
+				self::extract_tree(reader, ptr, &path, args)
+					.with_context(|| format!("Unable to extract directory {}", path.display()))?;
+
+				// Then set it's date
+				// Note: We must do this *after* extracting the tree, else the time
+				//       will be updated when we insert files into it.
+				if let Err(err) = filetime::set_file_mtime(&path, time) {
+					log::warn!(
+						"Unable to write date for directory {}: {}",
+						path.display(),
+						zutil::fmt_err_wrapper(&err)
+					);
+				}
+			},
+		}
+	}
+
+	Ok(())
+}
