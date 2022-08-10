0909a5c4c89faa525142cb356c0ebf07feff307c
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-08-10 18:11:51 +0100

    Replaced all hard-coded locations in `f2.s`

--- dcb-asm/todo/f2.s
+++ dcb-asm/todo/f2.s
@@ -23,27 +23,27 @@ f2:
 	lui $v1, 0x2000
 	or $v0, $v1
 	sw $v0, ($s0)
-	lui $v1, 0x8007
+	lui $v1, %hi(S_0x80077a1a)
 	lhui $v0, S_0x80077a18
 	nop
-	sh $v0, 0x7a1a($v1)
+	sh $v0, %lo(S_0x80077a1a)($v1)
 	sll $v0, 0x10
 	bnez $v0, .1
-		lui $v0, 0x8007
+		lui $v0, %hi(S_0x80077a10)
 	lwi $v0, S_0x80077a08
 	nop
 	bnez $v0, .3
 		nop
-	lui $v0, 0x8007
+	lui $v0, %hi(S_0x80077a14)
 	la_ $v1, S_0x80077ba0
-	sw $v1, 0x7a14($v0)
-	lui $v0, 0x8007
+	sw $v1, %lo(S_0x80077a14)($v0)
+	lui $v0, %hi(S_0x80077a1c)
 	j .3
-		sh $zr, 0x7a1c($v0)
+		sh $zr, %lo(S_0x80077a1c)($v0)
 .1:
-	sw $s0, 0x7a10($v0)
-	lui $a0, 0x8007
-	addiu $s0, $a0, 0x7ba0
+	sw $s0, %lo(S_0x80077a10)($v0)
+	lui $a0, %hi(S_0x80077ba0)
+	addiu $s0, $a0, %lo(S_0x80077ba0)
 	lui $v0, %hi(S_0x80077a0c)
 	sw $s0, %lo(S_0x80077a0c)($v0)
 	lui $v1, %hi(S_0x80077ba0)
