543e2299180c12ad21f8ffe3ecbb458e4f375d9f
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-08-10 22:26:28 +0100

    Replaced all hard-coded locations in `f51.s`.

--- dcb-asm/todo/f51.s
+++ dcb-asm/todo/f51.s
@@ -1,11 +1,11 @@
 f51:
 	addiu $sp, -0x18
 	sw $ra, 0x10($sp)
-	lui $a1, 0x8009
-	lui $v0, 0x8008
-	lw $a0, -0x6c60($v0)
-	lui $v0, 0x8009
-	lhu $v1, -0x6814($v0)
+	lui $a1, %hi(S_0x800897e8)
+	lui $v0, %hi(S_0x800793a0)
+	lw $a0, %lo(S_0x800793a0)($v0)
+	lui $v0, %hi(S_0x800897ec)
+	lhu $v1, %lo(S_0x800897ec)($v0)
 	nop
 	sll $v0, $v1, 0x2
 	addu $v0, $v1
@@ -15,7 +15,7 @@ f51:
 	lw $v1, 0x40bc($a0)
 	nop
 	addu $v0, $v1
-	lw $v1, -0x6818($a1)
+	lw $v1, %lo(S_0x800897e8)($a1)
 	nop
 	bne $v1, $v0, .0
 		move_ $v0, $zr
