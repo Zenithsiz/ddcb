c4a4cb65dfab2659d9d684134bdc97a6d9d17542
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-08-15 02:26:03 +0100

    Updated documentation on `dcb-pak`

--- tools/dcb-pak/src/lib.rs
+++ tools/dcb-pak/src/lib.rs
@@ -1,4 +1,16 @@
 //! `.PAK` files
+//!
+//! `.PAK` files seem be some kind of collection
+//! of other files, associated with an id (non-unique).
+//!
+//! They are composed of several files concatenated, with
+//! a header for each with their size, id and kind.
+//!
+//! The kind seems to describe which kind of file it represents,
+//! such as 3d animations, `MSD` files, audio (`seq`, `vh` and `vb`),
+//! and others.
+//!
+//! It is unknown what the id represents.
 
 // Features
 #![feature(never_type, unwrap_infallible, array_chunks)]
