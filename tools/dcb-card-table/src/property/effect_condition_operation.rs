deef7ca45e0ba7be4e15447a49d385ed9518e2de
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-10-02 09:53:13 +0100

    Replaced all uppercase hex characters with lowercase ones.

--- tools/dcb-card-table/src/property/effect_condition_operation.rs
+++ tools/dcb-card-table/src/property/effect_condition_operation.rs
@@ -62,7 +62,7 @@ impl Bytes for EffectConditionOperation {
 			5 => Ok(Self::EqualToNumber),
 			// TODO: Not do this here and just have someone above check?
 			0xff => {
-				log::warn!("Found byte 0xFF for effect condition operation. Interpreting as `EqualToNumber`");
+				log::warn!("Found byte 0xff for effect condition operation. Interpreting as `EqualToNumber`");
 				log::info!("The previous warning should only appear for \"Aquilamon\" in the original game file.");
 				Ok(Self::EqualToNumber)
 			},
