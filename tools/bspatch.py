5d06e3844fd6f2a9933687a456317051621c82ac
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-10-07 22:06:48 +0100

    All `bspatch`d files now save the intermediary file.

--- tools/bspatch.py
+++ tools/bspatch.py
@@ -4,18 +4,23 @@
 import os
 import subprocess
 import argparse
+import shutil
+
 
 def main(args):
 	# If `DCB_USE_BSPATCH` is defined and non-'1', just return
 	if os.environ.get('DCB_USE_BSPATCH') not in [None, '1']:
 		print("Skipping patching due to `DCB_USE_BSPATCH`")
+		shutil.copy(args.input, args.output)
 		return
 
 	# Else execute bspatch
 	subprocess.run(['bspatch', args.input, args.output, args.patch])
 
+
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description="Wrapper for `bspatch` that doesn't run if `DCB_USE_BSPATCH` exists and isn't `1`")
+	parser = argparse.ArgumentParser(
+	    description="Wrapper for `bspatch` that doesn't run if `DCB_USE_BSPATCH` exists and isn't `1`")
 	parser.add_argument("input", type=str)
 	parser.add_argument("output", type=str)
 	parser.add_argument("patch", type=str)
