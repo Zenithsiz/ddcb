5ab79754a401c45e65dc0b4b18ae5b443c941867
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-10-09 16:38:10 +0100

    `bspatch.py` can now handle missing patch files. Removed all useless patch files.

--- tools/bspatch.py
+++ tools/bspatch.py
@@ -16,16 +16,24 @@ def get_env(name):
 
 
 def main(args):
-	# If `DCB_USE_BSPATCH` is defined and non-'1', just return
+	# If `DCB_USE_BSPATCH` is defined and non-'1', just copy input to output and return
 	if get_env('DCB_USE_BSPATCH') not in [None, '1', 'yes']:
 		print("Skipping patching due to `DCB_USE_BSPATCH`")
-		shutil.copy(args.input, args.output)
+		if not os.path.samefile(args.input, args.output):
+			shutil.copy(args.input, args.output)
 		return
 
 	# Copy the original file to `$(input).unpatched`, if asked
 	if get_env('DCB_BSPATCH_SAVE_ORIGINAL') in ['1', 'yes']:
 		print("Copying unpatched file due to `DCB_BSPATCH_SAVE_ORIGINAL`")
 		shutil.copy(args.input, f"{args.input}.unpatched")
+
+	# If there's no patch file, just copy the file to the output, unless they're the same file
+	if not os.path.exists(args.patch):
+		print("Skipping patching due to patch missing")
+		if not os.path.samefile(args.input, args.output):
+			shutil.copy(args.input, args.output)
+		return
 
 	# Then execute bspatch
 	subprocess.run(['bspatch', args.input, args.output, args.patch])
