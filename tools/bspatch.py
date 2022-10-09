2a07c18bc456145ba62c4920453a57d09b914411
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2022-10-09 13:50:14 +0100

    Unpatched files are now created by `bspatch` instead and only optionally

--- tools/bspatch.py
+++ tools/bspatch.py
@@ -7,14 +7,27 @@ import argparse
 import shutil
 
 
+# Returns an environment variable lower case
+def get_env(name):
+	var = os.environ.get(name)
+	if var is not None:
+		var = var.lower()
+	return var
+
+
 def main(args):
 	# If `DCB_USE_BSPATCH` is defined and non-'1', just return
-	if os.environ.get('DCB_USE_BSPATCH') not in [None, '1']:
+	if get_env('DCB_USE_BSPATCH') not in [None, '1', 'yes']:
 		print("Skipping patching due to `DCB_USE_BSPATCH`")
 		shutil.copy(args.input, args.output)
 		return
 
-	# Else execute bspatch
+	# Copy the original file to `$(input).unpatched`, if asked
+	if get_env('DCB_BSPATCH_SAVE_ORIGINAL') in ['1', 'yes']:
+		print("Copying unpatched file due to `DCB_BSPATCH_SAVE_ORIGINAL`")
+		shutil.copy(args.input, f"{args.input}.unpatched")
+
+	# Then execute bspatch
 	subprocess.run(['bspatch', args.input, args.output, args.patch])
 
 
